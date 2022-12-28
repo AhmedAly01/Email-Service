177b770cca6d3910e239ef136713118cc86d0083
AhmedAly01 <96546133+AhmedAly01@users.noreply.github.com>
2022-12-28 23:23:59 +0200

    UI fixes

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AuthGuard} from "../guards/auth.guard";
 import {Router} from "@angular/router";
 import {SignedInAuthGuard} from "../guards/signed-in-auth.guard";
+import {UserService} from "../service/user/user-service.service";
+import {User} from "../models/user/user";
 
 @Component({
   selector: 'app-home',
@@ -9,13 +11,17 @@ import {SignedInAuthGuard} from "../guards/signed-in-auth.guard";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  user: string | undefined;
 
-  constructor(private router: Router, private authGuard: AuthGuard, private signedAuth: SignedInAuthGuard) { }
+  constructor(private service: UserService, private router: Router, private authGuard: AuthGuard, private signedAuth: SignedInAuthGuard) { }
 
   ngOnInit(): void {
     if (!this.authGuard.isSignedIn) {
       this.router.navigateByUrl('').then();
     }
+    this.service.user?.subscribe((data: User) => {
+      this.user = data.name
+    });
   }
 
   signOut() {
